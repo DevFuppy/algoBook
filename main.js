3107a47426c79bded967b9f1b6efc694aee7de58
JoeJoe <iiispan.jojo@gmail.com>
2025-09-05 01:30:36 +0800

    add Q

--- main.js
+++ main.js
@@ -286,3 +286,100 @@ const solveTwoSum = (arr, sum) => {
 // 以上 4 點改完，整體結構不變，複雜度仍是 O(n²)（因為 indexOf/splice），但邏輯就穩了：支援 0、重複值、負數，且「用掉就算」。
 
 // 如果你之後想把效能升到 O(n)，也不用重寫整個程式，只要把 expected 改成「值 → 欲配對次數」的 Map（或物件當計數表）即可，其餘流程一樣：命中就扣 1，歸零就刪鍵。
+
+
+
+/**Array Queue**/
+let front = 0
+let rear = 0
+
+let Q = []
+
+function enQ (int){
+
+  Q[rear] = int
+  rear++;
+  return Q
+}
+
+function dQ(){
+
+    if(front===rear)return'the Q is already empty'
+
+    let removed = Q[front]
+     Q[front] = undefined;
+    front++
+
+    return removed
+  
+}
+
+function showQ(){
+
+  return Q.slice(front,rear)
+
+}
+//front也是跑到下一個要拿掉的位置，但show從他開始顯示剛剛好。 rear是跑到下一個要加入的地方，但slice他剛好不計，所以放那邊剛好
+//包成方法? 可練習實作
+
+
+/**Node Queue**/
+function User(acc,pwd){
+
+    this.acc = acc;
+    this.pwd = pwd;
+    this.next =null;
+
+}
+
+let nFront = null;
+let nRear = null;
+
+function NenQ(acc,pwd){
+
+  let newNode = new User(acc,pwd)
+
+  if(nFront===null){
+ 
+   nFront = nRear = newNode;
+  
+  }
+  else{         
+      nRear.next = newNode
+      nRear =  nRear.next
+  }
+return nRear
+}
+
+function NdQ(){
+
+  let removed = nFront
+  nFront = nFront.next
+
+  return removed
+  
+}
+
+function NshowQ(){
+
+  let res = [];
+
+  let temp = nFront;
+
+  while(temp!==null)
+  {
+
+    res.push(temp)
+
+    temp = temp.next
+
+  }
+  
+  return res;
+}
+//加size維護
+//dq取光怎麼辦沒處理
+//處理rear，但其實Front都有控制，rear沒差(想想為何沒差)
+//看書怎麼寫
+//好好想想 rear,front, .next 的指派問題，想想為什麼front在有兩個node又enQ的時候，被改到為什麼沒差且反而需要
+//因為對於改.next來說，他只是要確定node可以連結，本來上一顆next就是要連到新node，不管他是不是第一個front，至於rear指幫本來就要必定移到最後一個node，這兩步並沒有衝突，且代表的意義不一樣，一個是接node，一個是參照要移動。一開始改到front本來就是應該的，因為就是要改到他連到第二顆進來的node，之後rear再移動就對了。
